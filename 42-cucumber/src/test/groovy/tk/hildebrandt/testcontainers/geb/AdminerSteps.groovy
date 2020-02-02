c5009d541a3f0d31e53e9805a82f8911d04b746a
Stefan Hildebrandt <stefan.hildebrandt.ext@ewe.de>
2020-02-02 21:41:18 +0100

    cleanup example

--- 42-cucumber/src/test/groovy/tk/hildebrandt/testcontainers/geb/AdminerSteps.groovy
+++ 42-cucumber/src/test/groovy/tk/hildebrandt/testcontainers/geb/AdminerSteps.groovy
@@ -8,22 +8,12 @@ import tk.hildebrandt.testcontainers.geb.pages.LoginPage
 
 class AdminerSteps extends GebEnabledStep {
 
-   TestContext testContext
-
    AdminerSteps(TestContext testContext) {
-      this.testContext = testContext
+      super(testContext)
    }
 
    @When('^user set valid credentials for current db in web browser')
    public void login() {
-      testContext.webDriverContainer = new BrowserWebDriverContainer()
-         .withCapabilities(new ChromeOptions())
-         .withRecordingMode(
-            BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL,
-            new File("out/cucumber"))
-         .withRecordingFileFactory(new CustomRecordingFileFactory())
-      testContext.webDriverContainer.withNetwork(testContext.network)
-      testContext.webDriverContainer.start()
       to(LoginPage).login(testContext.dbmsType)
    }
 }
