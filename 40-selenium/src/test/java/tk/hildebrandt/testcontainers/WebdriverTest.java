aab70a2c8187a4750340af2a5b50b9c3e8cbb3be
Stefan Hildebrandt <stefan.hildebrandt.ext@ewe.de>
2020-08-21 19:48:59 +0200

    update documentation a

--- 40-selenium/src/test/java/tk/hildebrandt/testcontainers/WebdriverTest.java
+++ 40-selenium/src/test/java/tk/hildebrandt/testcontainers/WebdriverTest.java
@@ -34,10 +34,10 @@ public class WebdriverTest {
       WebDriverWait wait = new WebDriverWait(webDriver, 10);
       wait.until(ExpectedConditions.elementToBeClickable(By.id("sb_form_q")));
       WebElement searchField = webDriver.findElement(By.id("sb_form_q"));
-      searchField.sendKeys("OOP Konferenz");
+      searchField.sendKeys("german testing day");
       webDriver.findElement(By.cssSelector("label[for=sb_form_go]")).click();
       WebElement conferenceLink =
          webDriver.findElement(By.cssSelector("li.b_algo h2 a"));
-      assertEquals("https://www.oop-konferenz.de/", conferenceLink.getAttribute("href"));
+      assertEquals("https://www.germantestingday.info/", conferenceLink.getAttribute("href"));
    }
 }
