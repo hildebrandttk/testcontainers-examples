4c1bd7425a97070fcb7a5f7691b284a51823a014
Stefan Hildebrandt <stefan.hildebrandt.ext@ewe.de>
2020-08-24 19:59:44 +0200

    use junit 5 for recording test

--- 41-selenium-recording/src/test/java/tk/hildebrandt/testcontainers/WebdriverRecordingTest.java
+++ 41-selenium-recording/src/test/java/tk/hildebrandt/testcontainers/WebdriverRecordingTest.java
@@ -2,9 +2,7 @@ package tk.hildebrandt.testcontainers;
 
 import java.io.File;
 
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -13,9 +11,12 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static junit.framework.TestCase.assertEquals;
 
+@Testcontainers
 public class WebdriverRecordingTest {
 
    static {
@@ -24,11 +25,10 @@ public class WebdriverRecordingTest {
       SLF4JBridgeHandler.install();
    }
 
-   @Rule
+   @Container
    public BrowserWebDriverContainer CHROME =
       new BrowserWebDriverContainer()
          .withCapabilities(new ChromeOptions())
-         //TODO withRecordingMode didn't work with junit5
          .withRecordingMode(
             BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL,
             new File("."));
