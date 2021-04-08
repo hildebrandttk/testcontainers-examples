5df785964ff7b0232de31119cbe9df00bf4a0ca5
Stefan Hildebrandt <stefan.hildebrandt.ext@ewe.de>
2021-04-08 20:35:59 +0200

    Update dependencies.

--- 34-spring-boot-embedded/src/test/java/tk/hildebrandt/testcontainers/springboot/user/DatabaseTestConfiguration.java
+++ 34-spring-boot-embedded/src/test/java/tk/hildebrandt/testcontainers/springboot/user/DatabaseTestConfiguration.java
@@ -8,6 +8,9 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
+/**
+https://github.com/Playtika/testcontainers-spring-boot
+*/
 @TestConfiguration
 @EnableJdbcRepositories
 public class DatabaseTestConfiguration {
