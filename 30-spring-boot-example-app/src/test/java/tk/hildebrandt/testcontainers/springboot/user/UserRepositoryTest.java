7a7e8925310e6797d4e6410a9f078f615c53ab76
Stefan Hildebrandt <stefan.hildebrandt.ext@ewe.de>
2021-04-08 20:35:59 +0200

    Update dependencies.
    (cherry picked from commit 5df785964ff7b0232de31119cbe9df00bf4a0ca5)

--- 30-spring-boot-example-app/src/test/java/tk/hildebrandt/testcontainers/springboot/user/UserRepositoryTest.java
+++ 30-spring-boot-example-app/src/test/java/tk/hildebrandt/testcontainers/springboot/user/UserRepositoryTest.java
@@ -2,18 +2,17 @@ package tk.hildebrandt.testcontainers.springboot.user;
 
 import java.util.List;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import tk.hildebrandt.testcontainers.springboot.config.DataRepositoryConfiguration;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 @Transactional
 @ContextConfiguration(classes = {DatabaseTestConfiguration.class,
    DataRepositoryConfiguration.class})
